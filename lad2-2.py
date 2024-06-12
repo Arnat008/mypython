a844e97cf4dfe61835d25aaea09501950bc1cceb
Arnat008 <6629011008@cdti.ac.th>
2024-06-12 15:21:26 +0700

    update

---
+++ lad2-2.py
@@ -0,0 +1,19 @@
+A = int(input("โปรดป้อนคะแนนของคุณ: "))  
+
+if A >= 80 and A  <= 100 :  
+    print("เกรด A")
+     
+elif  A >= 70 and A  <= 79 :  
+    print("เกรด B")
+    
+elif  A >= 60 and A  <= 69 :
+    print("เกรด C")
+    
+elif  A >= 50 and A  <= 59 :
+    print("เกรด D")
+
+elif  A >= 0 and A  <= 49 :
+    print("เกรด F")
+else:
+    print("กรุณากรอกข้อมูลหใม่ให้ถูกต้อง น่ะฮ๊าฟไออ้วนนนนนนน")  
+
